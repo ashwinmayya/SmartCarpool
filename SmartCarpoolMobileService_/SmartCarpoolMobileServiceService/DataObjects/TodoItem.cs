b284bc8f5e88f3ecad86a1ed2e1ebb4f34601bf6
bikramsenmicrosoft <bikramsen@live.com>
2015-07-24 16:41:08 -0700

    Smart Carpool Backend

---
+++ SmartCarpoolMobileService_/SmartCarpoolMobileServiceService/DataObjects/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.WindowsAzure.Mobile.Service;
+
+namespace SmartCarpoolMobileServiceService.DataObjects
+{
+    public class TodoItem : EntityData
+    {
+        public string Text { get; set; }
+
+        public bool Complete { get; set; }
+    }
+}
