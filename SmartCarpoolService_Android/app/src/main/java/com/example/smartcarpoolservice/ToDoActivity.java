09fb29e98a7c36718bf2d38c564ade5937d8f9ee
ashwinmayya <ashwinm@microsoft.com>
2015-07-27 01:18:05 -0700

    Added Google Authentication to sample app

--- SmartCarpoolService_Android/app/src/main/java/com/example/smartcarpoolservice/ToDoActivity.java
+++ SmartCarpoolService_Android/app/src/main/java/com/example/smartcarpoolservice/ToDoActivity.java
@@ -24,6 +24,7 @@ import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.SettableFuture;
 import com.microsoft.windowsazure.mobileservices.MobileServiceClient;
+import com.microsoft.windowsazure.mobileservices.UserAuthenticationCallback;
 import com.microsoft.windowsazure.mobileservices.http.NextServiceFilterCallback;
 import com.microsoft.windowsazure.mobileservices.http.ServiceFilter;
 import com.microsoft.windowsazure.mobileservices.http.ServiceFilterRequest;
@@ -39,6 +40,17 @@ import com.microsoft.windowsazure.mobileservices.table.sync.localstore.SQLiteLoc
 import com.microsoft.windowsazure.mobileservices.table.sync.synchandler.SimpleSyncHandler;
 
 import static com.microsoft.windowsazure.mobileservices.table.query.QueryOperations.*;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.ExecutionException;
+import com.microsoft.windowsazure.mobileservices.MobileServiceException;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+
+import com.microsoft.windowsazure.mobileservices.authentication.MobileServiceAuthenticationProvider;
+import com.microsoft.windowsazure.mobileservices.authentication.MobileServiceUser;
 
 public class ToDoActivity extends Activity {
 
@@ -73,14 +85,21 @@ public class ToDoActivity extends Activity {
      */
     private ProgressBar mProgressBar;
 
+    public static final String SHAREDPREFFILE = "temp";
+    public static final String USERIDPREF = "uid";
+    public static final String TOKENPREF = "tkn";
+
+    public boolean bAuthenticating = false;
+    public final Object mAuthenticationLock = new Object();
+
     /**
      * Initializes the activity
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_to_do);
 
+        setContentView(R.layout.activity_to_do);
         mProgressBar = (ProgressBar) findViewById(R.id.loadingProgressBar);
 
         // Initialize the progress bar
@@ -88,37 +107,19 @@ public class ToDoActivity extends Activity {
 
         try {
             // Create the Mobile Service Client instance, using the provided
-
             // Mobile Service URL and key
             mClient = new MobileServiceClient(
                     "https://smartcarpoolservice.azure-mobile.net/",
-                    "LVxoMLwxEaIYGtRCmtbgLYGPwLuIMJ27",
-                    this).withFilter(new ProgressFilter());
+                    "LVxoMLwxEaIYGtRCmtbgLYGPwLuIMJ27", this)
+                    .withFilter(new ProgressFilter())
+                    .withFilter(new RefreshTokenCacheFilter());
 
-            // Get the Mobile Service Table instance to use
-
-            mToDoTable = mClient.getTable(ToDoItem.class);
-
-            // Offline Sync
-            //mToDoTable = mClient.getSyncTable("ToDoItem", ToDoItem.class);
-
-            //Init local storage
-            initLocalStore().get();
-
-            mTextNewToDo = (EditText) findViewById(R.id.textNewToDo);
-
-            // Create an adapter to bind the items with the view
-            mAdapter = new ToDoItemAdapter(this, R.layout.row_list_to_do);
-            ListView listViewToDo = (ListView) findViewById(R.id.listViewToDo);
-            listViewToDo.setAdapter(mAdapter);
-
-            // Load the items from the Mobile Service
-            refreshItemsFromTable();
+            // Authenticate passing false to load the current token cache if available.
+            authenticate(false);
 
         } catch (MalformedURLException e) {
-            createAndShowDialog(new Exception("There was an error creating the Mobile Service. Verify the URL"), "Error");
-        } catch (Exception e){
-            createAndShowDialog(e, "Error");
+            createAndShowDialog(new Exception("Error creating the Mobile Service. " +
+                    "Verify the URL"), "Error");
         }
     }
 
@@ -477,6 +478,225 @@ public class ToDoActivity extends Activity {
             });
 
             return resultFuture;
+        }
+    }
+
+    /**
+     * Authenticates with the desired login provider. Also caches the token.
+     *
+     * If a local token cache is detected, the token cache is used instead of an actual
+     * login unless bRefresh is set to true forcing a refresh.
+     *
+     * @param bRefreshCache
+     *            Indicates whether to force a token refresh.
+     */
+     private void authenticate(boolean bRefreshCache) {
+
+         bAuthenticating = true;
+
+         if (bRefreshCache || !loadUserTokenCache(mClient)) {
+             // New login using the provider and update the token cache.
+             mClient.login(MobileServiceAuthenticationProvider.Google,
+                     new UserAuthenticationCallback() {
+                         @Override
+                         public void onCompleted(MobileServiceUser user,
+                                                 Exception exception, ServiceFilterResponse response) {
+
+                             synchronized (mAuthenticationLock) {
+                                 if (exception == null) {
+                                     cacheUserToken(mClient.getCurrentUser());
+                                     createTable();
+                                 } else {
+                                     createAndShowDialog(exception.getMessage(), "Login Error");
+                                 }
+                                 bAuthenticating = false;
+                                 mAuthenticationLock.notifyAll();
+                             }
+                         }
+                     });
+         } else {
+             // Other threads may be blocked waiting to be notified when
+             // authentication is complete.
+             synchronized (mAuthenticationLock) {
+                 bAuthenticating = false;
+                 mAuthenticationLock.notifyAll();
+             }
+             createTable();
+         }
+     }
+
+
+    private void createTable(){
+        try {
+            // Get the Mobile Service Table instance to use
+
+            mToDoTable = mClient.getTable(ToDoItem.class);
+
+            // Offline Sync
+            //mToDoTable = mClient.getSyncTable("ToDoItem", ToDoItem.class);
+
+            //Init local storage
+            initLocalStore().get();
+
+            mTextNewToDo = (EditText) findViewById(R.id.textNewToDo);
+
+            // Create an adapter to bind the items with the view
+            mAdapter = new ToDoItemAdapter(this, R.layout.row_list_to_do);
+            ListView listViewToDo = (ListView) findViewById(R.id.listViewToDo);
+            listViewToDo.setAdapter(mAdapter);
+
+            // Load the items from the Mobile Service
+            refreshItemsFromTable();
+        }
+        catch (Exception e){
+            createAndShowDialog(e, "Error");
+        }
+    }
+
+    private void cacheUserToken(MobileServiceUser user)
+    {
+        SharedPreferences prefs = getSharedPreferences(SHAREDPREFFILE, Context.MODE_PRIVATE);
+        Editor editor = prefs.edit();
+        editor.putString(USERIDPREF, user.getUserId());
+        editor.putString(TOKENPREF, user.getAuthenticationToken());
+        editor.commit();
+    }
+
+    private boolean loadUserTokenCache(MobileServiceClient client)
+    {
+        SharedPreferences prefs = getSharedPreferences(SHAREDPREFFILE, Context.MODE_PRIVATE);
+        String userId = prefs.getString(USERIDPREF, "undefined");
+        if (userId == "undefined")
+            return false;
+        String token = prefs.getString(TOKENPREF, "undefined");
+        if (token == "undefined")
+            return false;
+
+        MobileServiceUser user = new MobileServiceUser(userId);
+        user.setAuthenticationToken(token);
+        client.setCurrentUser(user);
+
+        return true;
+    }
+
+    /**
+     * Detects if authentication is in progress and waits for it to complete.
+     * Returns true if authentication was detected as in progress. False otherwise.
+     */
+    public boolean detectAndWaitForAuthentication()
+    {
+        boolean detected = false;
+        synchronized(mAuthenticationLock)
+        {
+            do
+            {
+                if (bAuthenticating == true)
+                    detected = true;
+                try
+                {
+                    mAuthenticationLock.wait(1000);
+                }
+                catch(InterruptedException e)
+                {}
+            }
+            while(bAuthenticating == true);
+        }
+        if (bAuthenticating == true)
+            return true;
+
+        return detected;
+    }
+
+    /**
+     * Waits for authentication to complete then adds or updates the token
+     * in the X-ZUMO-AUTH request header.
+     *
+     * @param request
+     *            The request that receives the updated token.
+     */
+    private void waitAndUpdateRequestToken(ServiceFilterRequest request)
+    {
+        MobileServiceUser user = null;
+        if (detectAndWaitForAuthentication())
+        {
+            user = mClient.getCurrentUser();
+            if (user != null)
+            {
+                request.removeHeader("X-ZUMO-AUTH");
+                request.addHeader("X-ZUMO-AUTH", user.getAuthenticationToken());
+            }
+        }
+    }
+
+    /**
+     * The RefreshTokenCacheFilter class filters responses for HTTP status code 401.
+     * When 401 is encountered, the filter calls the authenticate method on the
+     * UI thread. Out going requests and retries are blocked during authentication.
+     * Once authentication is complete, the token cache is updated and
+     * any blocked request will receive the X-ZUMO-AUTH header added or updated to
+     * that request.
+     */
+    private class RefreshTokenCacheFilter implements ServiceFilter {
+
+        AtomicBoolean mAtomicAuthenticatingFlag = new AtomicBoolean();
+
+        @Override
+        public ListenableFuture<ServiceFilterResponse> handleRequest(
+                final ServiceFilterRequest request,
+                final NextServiceFilterCallback nextServiceFilterCallback
+        )
+        {
+            // In this example, if authentication is already in progress we block the request
+            // until authentication is complete to avoid unnecessary authentications as
+            // a result of HTTP status code 401.
+            // If authentication was detected, add the token to the request.
+            waitAndUpdateRequestToken(request);
+
+            // Send the request down the filter chain
+            // retrying up to 5 times on 401 response codes.
+            ListenableFuture<ServiceFilterResponse> future = null;
+            ServiceFilterResponse response = null;
+            int responseCode = 401;
+            for (int i = 0; (i < 5 ) && (responseCode == 401); i++)
+            {
+                future = nextServiceFilterCallback.onNext(request);
+                try {
+                    response = future.get();
+                    responseCode = response.getStatus().getStatusCode();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    if (e.getCause().getClass() == MobileServiceException.class)
+                    {
+                        MobileServiceException mEx = (MobileServiceException) e.getCause();
+                        responseCode = mEx.getResponse().getStatus().getStatusCode();
+                        if (responseCode == 401)
+                        {
+                            // Two simultaneous requests from independent threads could get HTTP status 401.
+                            // Protecting against that right here so multiple authentication requests are
+                            // not setup to run on the UI thread.
+                            // We only want to authenticate once. Requests should just wait and retry
+                            // with the new token.
+                            if (mAtomicAuthenticatingFlag.compareAndSet(false, true))
+                            {
+                                // Authenticate on UI thread
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        // Force a token refresh during authentication.
+                                        authenticate(true);
+                                    }
+                                });
+                            }
+
+                            // Wait for authentication to complete then update the token in the request.
+                            waitAndUpdateRequestToken(request);
+                            mAtomicAuthenticatingFlag.set(false);
+                        }
+                    }
+                }
+            }
+            return future;
         }
     }
 }
