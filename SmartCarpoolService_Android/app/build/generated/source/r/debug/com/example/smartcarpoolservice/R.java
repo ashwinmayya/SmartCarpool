56e63e2c47e95703f6a264c158b52bc95a6044a7
bikramsenmicrosoft <bikramsen@live.com>
2015-07-27 15:37:32 -0700

    Adding the Driver/passenger screen
    v 1.0

--- SmartCarpoolService_Android/app/build/generated/source/r/debug/com/example/smartcarpoolservice/R.java
+++ SmartCarpoolService_Android/app/build/generated/source/r/debug/com/example/smartcarpoolservice/R.java
@@ -8,35 +8,57 @@
 package com.example.smartcarpoolservice;
 
 public final class R {
+    public static final class array {
+        public static final int timerange=0x7f040000;
+    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int LinearLayout1=0x7f070000;
-        public static final int buttonAddToDo=0x7f070004;
-        public static final int checkToDoItem=0x7f070006;
-        public static final int listViewToDo=0x7f070005;
-        public static final int loadingProgressBar=0x7f070002;
-        public static final int menu_refresh=0x7f070007;
-        public static final int textNewToDo=0x7f070003;
-        public static final int textViewTitle=0x7f070001;
+        public static final int LinearLayout1=0x7f080000;
+        public static final int app_title=0x7f080007;
+        public static final int background_grid=0x7f080006;
+        public static final int button=0x7f080009;
+        public static final int buttonAddToDo=0x7f080004;
+        public static final int checkToDoItem=0x7f080011;
+        public static final int destinationlabel=0x7f08000c;
+        public static final int destinationtext=0x7f08000d;
+        public static final int listViewToDo=0x7f080005;
+        public static final int loadingProgressBar=0x7f080002;
+        public static final int menu_refresh=0x7f080012;
+        public static final int ok_button=0x7f080010;
+        public static final int sourcelabel=0x7f08000a;
+        public static final int sourcetext=0x7f08000b;
+        public static final int spinner=0x7f08000f;
+        public static final int textNewToDo=0x7f080003;
+        public static final int textViewTitle=0x7f080001;
+        public static final int timelabel=0x7f08000e;
+        public static final int toggleButton=0x7f080008;
     }
     public static final class layout {
         public static final int activity_to_do=0x7f030000;
-        public static final int row_list_to_do=0x7f030001;
+        public static final int driver_main_page=0x7f030001;
+        public static final int row_list_to_do=0x7f030002;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_main=0x7f070000;
     }
     public static final class string {
-        public static final int add_button_text=0x7f040000;
-        public static final int add_textbox_hint=0x7f040001;
-        public static final int app_name=0x7f040002;
-        public static final int checkbox_text=0x7f040003;
-        public static final int menu_refresh=0x7f040004;
-        public static final int mobile_services=0x7f040005;
+        public static final int add_button_text=0x7f050000;
+        public static final int add_textbox_hint=0x7f050001;
+        public static final int app_mode_driver=0x7f050002;
+        public static final int app_mode_passenger=0x7f050003;
+        public static final int app_name=0x7f050004;
+        public static final int app_title=0x7f050005;
+        public static final int checkbox_text=0x7f050006;
+        public static final int destination_label=0x7f050007;
+        public static final int menu_refresh=0x7f050008;
+        public static final int mobile_services=0x7f050009;
+        public static final int ok_button_text=0x7f05000a;
+        public static final int source_label=0x7f05000b;
+        public static final int time_label=0x7f05000c;
     }
     public static final class style {
         /** 
@@ -47,9 +69,9 @@ public final class R {
  API 11 theme customizations can go here. 
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
---
+++ SmartCarpoolService_Android/app/src/main/java/com/example/smartcarpoolservice/SmartCarpoolHomepage.java
@@ -0,0 +1,17 @@
+package com.example.smartcarpoolservice;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by bisen on 7/27/2015.
+ */
+public class SmartCarpoolHomepage extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.driver_main_page);
+    }
+}
